758707093482011a7540238819249134fd3098d1
Anthony Yu <any027@ucsd.edu>
2020-07-27 21:13:33 -0700

    Comments

--- pytest_venv/demo/demo.py
+++ pytest_venv/demo/demo.py
@@ -5,6 +5,7 @@ class Demo():
     def add(self, a,b):
         return a + b
 
+    #Almost like read file
     def read_json(self, file_path):
         with open(file_path, "r") as f:
             return json.load(f)
